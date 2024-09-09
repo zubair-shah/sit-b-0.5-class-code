c7a5e15a5ac358907874c75ba1c5e81cb50cbf80
zubair-shah <shahzubair269@gmail.com>
2024-09-09 13:32:04 +0500

    updated code

---
+++ module-2/app.js
@@ -0,0 +1,41 @@
+/// Alert
+// alert("Bismillah")
+
+/// Prompt
+
+// prompt("Enter You Name:")
+
+// console.log(Math.floor(Math.random() * 10));
+
+// document.write("Hi i am from javascript")
+
+// document.getElementById("heading").innerHTML = "Zubair";
+document.getElementById("random-number").innerHTML = Math.floor(Math.random() * 10)
+
+
+// var myName = prompt("Enter You Name:");
+// let myName = prompt("Enter You Name:");
+// const myName = prompt("Enter You Name:");
+// const myName = "zubair";
+// myName = "shaikh"
+
+// document.getElementById("heading").innerHTML = myName;
+// console.log(myName)
+
+// let parentsTeachersNote = 16;
+
+// let myName = "zubair1";
+// let myName2 = 'zubair2';
+// let myName3 = `zubair3`;
+// console.log(myName)
+// console.log(myName2)
+// console.log(myName3)
+// let myAge = 24;
+// let currentSemester = 5;
+
+// let mydata = "my name is zubair and my age is " + myAge + "\ncurrently i am studying in " + currentSemester + "th semester";
+// console.log(mydata)
+
+// let myNewData = `my name is zubair and my age is ${myAge}
+// currently i am studying in ${currentSemester}th semester`
+// console.log(myNewData)
